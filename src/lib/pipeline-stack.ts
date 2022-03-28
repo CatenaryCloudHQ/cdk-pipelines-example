96ec4a19707aea9abcaed7cdef15090198e8230c
Roman Naumenko <62673610+OperationalFallacy@users.noreply.github.com>
2022-03-27 20:05:29 -0400

    Bare bone pipeline

---
+++ src/lib/pipeline-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { Construct } from 'constructs';
+
+export class MyPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('CatenaryCloudHQ/cdk-pipelines-example', 'main', {
+          authentication: cdk.SecretValue.secretsManager('github-token'),
+        }),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      }),
+    });
+  }
+}
