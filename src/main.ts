f2ab9f3b69008b9cc8bcf3d8262f670846e1b14b
Roman Naumenko <roman@catenary.cloud>
2022-04-10 15:31:55 -0400

    Set account for pipeline with CDK_DEFAULT_ACCOUNT

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { App } from "aws-cdk-lib";
+import { App, Aws } from "aws-cdk-lib";
 import { MyPipelineStack } from "./lib/pipeline-stack";
 
 const app = new App();
+console.log(Aws.REGION, Aws.ACCOUNT_ID);
 
 new MyPipelineStack(app, "MyPipelineStack", {
   env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
     region: "us-east-1",
   },
 });
